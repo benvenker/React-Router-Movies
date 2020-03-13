3cb007e1bd2d4ba2b433b79f8d6723bb208020a0
Ben Venker <bvemails@gmail.com>
2020-03-12 22:05:43 -0700

    complete part II and  refactor stretch

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-
-      <Route exact path="/" component={MovieList}></Route>
-      <Route path="movies/:id" component={Movie}></Route>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
     </div>
   );
 };

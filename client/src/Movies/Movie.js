e98617a74fc05c3167e0ac9e14a11bb7ec7f4825
Ben Venker <bvemails@gmail.com>
2020-05-06 18:56:43 -0500

    use useParams hook and add spinner to movie page

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import Loader from "react-loader-spinner";
 
-const Movie = props => {
+const Movie = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
   const movieMatch = props.match;
   const [movie, setMovie] = useState();
+  const params = useParams();
 
   useEffect(() => {
-    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
     axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then(response => {
+      .get(`http://localhost:5000/api/movies/${params.id}`)
+      .then((response) => {
         setMovie(response.data);
+        setIsLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, [movie]);
@@ -28,7 +32,15 @@ const Movie = props => {
   };
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return (
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000}
+      />
+    );
   }
 
   const { title, director, metascore, stars } = movie;
